e884d4d9e96551a7f809181628a958c6080927ba
Mwadali <wellingtonmwadali@gmail.com>
2023-12-15 17:31:27 +0300

    routes  configs

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8080;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/auth', authRoutes);
+
+// Connect to MongoDB
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ server/routes/auth.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// Register a new user
+router.post('/signup', async (req, res) => {
+  try {
+    const { firstName, lastName, country, email, password } = req.body;
+
+    // Check if the email is already registered
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email is already registered' });
+    }
+
+    // Create a new user
+    const newUser = new User({ firstName, lastName, country, email, password });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    // Check the password
+    const isPasswordValid = await user.comparePassword(password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    res.json({ message: 'Login successful' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Check if email is already registered
+router.get('/check-email/:email', async (req, res) => {
+  try {
+    const { email } = req.params;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return res.status(200).json({ message: 'Email is registered' });
+      }
+  
+      res.status(404).json({ message: 'Email is not registered' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+  module.exports = router;
+  
