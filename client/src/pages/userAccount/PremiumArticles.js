8b5ccc5c54f59c562cde2b6dd7f2c1d6f5de966d
Mwadali <wellingtonmwadali@gmail.com>
2024-02-01 00:47:56 +0300

    created premium articles page

--- client/src/pages/userAccount/PremiumArticles.js
+++ client/src/pages/userAccount/PremiumArticles.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import Navbar from '../../components/navbar'
 import Sidebar from '../../components/Dashboard/sidebar'
-
+import NewsList from '../../components/newslist';
 
 const PremiumArticles = () => {
   return (
-    <>
-    <Navbar/>
-    <Sidebar/>
-    <div></div>
-    </>
-  )
+    <div>
+      <Navbar />
+      <div className='flex'>
+     <Sidebar />
+      <div className='md:flex md:flex-col md:w-4/5 p-2"'>
+       <div className='mt-4'>
+       <h1 className='font-bold text-xl'>Premium Articles</h1>
+        <p className='bg-slate-200 p-2'>
+          Discover a world of in-depth insights and strategic perspectives with
+          Alpha Trade's premium articles. Our premium content goes beyond the
+          surface, offering exclusive analyses, expert opinions, and deep dives
+          into the intricate dynamics of commodities, financial markets, and
+          global industries. Tailored for those seeking a comprehensive
+          understanding, these articles provide unique perspectives, market
+          forecasts, and actionable strategies. From detailed commodity market
+          reports to exclusive interviews with industry leaders, our premium
+          articles offer a premium experience, empowering you with the knowledge
+          needed to navigate and thrive in the complex landscape of global trade
+          and finance. Elevate your decision-making with Alpha Trade's premium
+          content – where expertise meets innovation.
+        </p>
+       </div>
+        <NewsList/>
+      </div>
+      </div>
+      
+    </div>
+  );
 }
 
 export default PremiumArticles
